275fbc47475aa65722a99ec7309880fa3797f361
Jeffrey Geer <viriveri@gmail.com>
2024-05-24 23:37:22 -0500

    fix: added missing hyprcursor compilation step

--- build.sh
+++ build.sh
@@ -79,14 +79,16 @@ create_aliases() {
 create_hyprcursors() {
 	local src_dir="$1"
 	local out_dir="$2"
+	local theme_name="$3"
+	local theme_comment="$4"
 	local hypr_dir="$out_dir/hyprcursors"
-	local filename dir cursor_dir striped base_name
+	local filename dir cursor_dir striped base_name symlink target cursor_dir
 
 	[ -d "$src_dir" ] || return 1
 	[ -d "$out_dir" ] || mkdir -p "$out_dir"
 	[ -d "$hypr_dir" ] || mkdir -p "$hypr_dir"
 
-	echo -ne "Copying hyprcursors...\\r"
+	echo -ne "Generating hyprcursor theme...\\r"
 	for file in "$src_dir"/*.svg; do
 		filename=$(basename -- "$file")
 
@@ -111,9 +113,7 @@ create_hyprcursors() {
 			cp -- "$file" "$cursor_dir"
 		fi
 	done
-	echo -e "Copying hyprcursors... DONE"
 
-	echo -ne "Generating hyprcursor theme...\\r"
 	for config in "$CONFIG_DIR"/*.hl; do
 		[ -f "$config" ] || continue
 
@@ -122,21 +122,38 @@ create_hyprcursors() {
 		[ -d "$cursor_dir" ] || continue
 		cp "$config" "$cursor_dir/meta.hl"
 	done
-	echo -e "Generating hyprcursor theme... DONE"
-}
 
-create_hyprcursor_aliases() {
-	local out_dir="$1"
-	local symlink target cursor_dir
-
-	echo -ne "Generating hyprcursor shortcuts...\\r"
 	while read -r symlink target; do
 		cursor_dir="$out_dir/$target"
 		if [ -f "$cursor_dir"/meta.hl ]; then
 			echo "define_override = $symlink" >>"$cursor_dir"/meta.hl
 		fi
 	done <"$ALIASES"
-	echo -e "Generating hyprcursor shortcuts... DONE"
+
+	theme_comment="$(grep Comment "$src_dir"/index.theme)"
+	cat >"$out_dir"/manifest.hl <<- EOM
+		name = ${theme_name%-Cursors}
+		description = ${theme_comment#Comment=}
+		version = $(git show -s --format=%cd --date=format:%Y%m%d HEAD)
+		cursors_directory = hyprcursors
+	EOM
+	echo -e "Generating hyprcursor theme... DONE"
+}
+
+compile_hyprcursors() {
+	local src_dir="$1"
+	local out_dir="$2"
+	local theme_name="$(basename $1)"
+	local compile_dir="$1/../theme_${theme_name%-Cursors}"
+	local filename dir cursor_dir striped base_name symlink target cursor_dir
+
+	[ -d "$src_dir" ] || return 1
+	[ -d "$out_dir" ] || mkdir -p "$out_dir"
+
+	echo -ne "Compiling hyprcursors...\\r"
+	hyprcursor-util -c "$src_dir" >/dev/null
+	cp -a "$compile_dir/." "$out_dir/"
+	echo -e "Compiling hyprcursors... DONE"
 }
 
 SCRIPT_DIR="$(dirname "$0")"
@@ -159,15 +176,8 @@ for theme_src_dir in "$SRC_DIR"/*; do
 	convert_to_x11cursor "$theme_build_dir" "$theme_out_dir"/cursors
 	create_aliases "$theme_out_dir"/cursors
 
-	create_hyprcursors "$theme_src_dir" "$theme_out_dir"
-	create_hyprcursor_aliases "$theme_out_dir"/hyprcursors
+	create_hyprcursors "$theme_src_dir" "$theme_build_dir" "$theme_name"
+	compile_hyprcursors "$theme_build_dir" "$theme_out_dir"
 
 	cp -f "$theme_src_dir/index.theme" "$theme_out_dir"/
-	theme_comment="$(grep Comment "$theme_src_dir"/index.theme)"
-	cat >"$theme_out_dir"/manifest.hl <<- EOM
-		name = $theme_name
-		description = ${theme_comment#Comment=}
-		version = $(git show -s --format=%cd --date=format:%Y%m%d HEAD)
-		cursors_directory = hyprcursors
-	EOM
 done
